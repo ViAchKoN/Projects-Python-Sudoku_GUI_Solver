c655c6b877ad9c30d51adb9306250c15e1d82463
17682639 <SbEr#1507>
2020-08-31 17:58:03 +0300

    Update for code

--- main.py
+++ main.py
@@ -12,19 +12,30 @@ class Grid:
     def __init__(self: object, grid: tuple):
         self.grid = grid
         self.size = len(grid[0])
-        self.subgrid_coordinates = {}
+        self.empty_cells = []
+        self.subgrids = {}
 
-    def get_subgrid_coordinates(self: object) -> None:
+    def get_subgrids(self: object) -> None:
         '''
         Get the coordinates of cells in subgrids and add them
         to the attributes of a Grid's exemplar
-        :return:
+        :return: Filled attribute of a grid with coordinates of cells which are in subgrids
         '''
         size = int(self.size/3)
         for k in range(size):
             for x in range(0, size):
                 rows = tuple([i, 0] for i in range(0+3*k, size+3*k))
-                self.subgrid_coordinates[(k, x)] = tuple((i[0], i[1]) for i in rows for i[1] in range(0+size*x, 3+size*x))
+                self.subgrids[(k, x)] = tuple((i[0], i[1]) for i in rows for i[1] in range(0+size*x, 3+size*x))
+
+    def get_empty_cells(self: object) -> None:
+        '''
+        Get the coordinates of cells which need to be filled
+        :return: Filled attribute of a grid with the list of cells which are empty
+        '''
+        empty_cells = []
+        for row in range(self.size):
+            empty_cells += [(row, i) for i in [col for col, x in enumerate(self.grid[row]) if x == 0]]
+        self.empty_cells = empty_cells
 
     def update_cell_value(self: object, cell: tuple, number: int) -> None:
         '''
@@ -35,13 +46,44 @@ class Grid:
         '''
         self.grid[cell[0]][cell[1]] = number
 
-    def rollback_cell_value(self: object, cell: tuple) -> None:
+    def zero_cell_value(self: object, cell: tuple) -> None:
         '''
-        Placeholder fot the function to go back to a previous cell and restore its last value
+        Set a cell value as 0
         :param cell: Coordinates of a cell
         :return: None
         '''
-        pass
+        self.grid[cell[0]][cell[1]] = 0
+
+    def rollback_cell_value(self: object, cell: tuple, available_numbers: dict):
+        '''
+        Update a cell value to a given number
+        :param cell: Coordinates of a cell
+        :param number: A number with which update a cell
+        :return: None
+        '''
+
+        n = 0
+
+        def rollback(grid: object, cell: tuple = cell, n=n):
+            available_numbers[cell] = list(range(1, grid.size + 1))
+            cell = grid.empty_cells[grid.empty_cells.index(cell) - 1]
+
+            n += 1
+            j = grid.get_cell_value(cell)
+
+            try:
+                grid.zero_cell_value(cell)
+                available_numbers[cell].remove(j)
+                if len(list(available_numbers.values())[0]) == 0:
+                    print('done')
+                if len(available_numbers[cell]) == 0:
+                    return rollback(grid, cell, n)
+            except ValueError:
+                return rollback(grid, cell, n)
+            else:
+                return cell, n, j,  available_numbers
+
+        return rollback(self, cell, n)
 
     def get_cell_value(self: object, cell: tuple) -> int:
         '''
@@ -67,17 +109,8 @@ class Grid:
         '''
         return [row[column_number] for row in self.grid]
 
-    def get_empty_cells(self: object) -> list:
-        '''
-        Get the coordinates of cells which need to be filled
-        :return: List of a cells which need to be filled
-        '''
-        empty_cells = []
-        for row in range(self.size):
-            empty_cells += [(row, i) for i in [col for col, x in enumerate(self.grid[row]) if x == 0]]
-        return empty_cells
-
-    def get_sum_subgrid(self: object) -> int:
+    def get_subgrid(self: object) -> int:
+        'TODO CHANGE FUNCTION TO GET SUBGRID'
         '''
         Get the total sum of the values of a subgrid
         :return: Sum of the values
@@ -110,70 +143,85 @@ class Grid:
         return not num in set(self.get_column(column_number))
 
 
-    def check_subgrid_unique(self: object, cell: tuple) -> tuple:
-        self.get_subgrid_coordinates()
-
-
-        rows = tuple([i, 0] for i in range(3))
-        subgrid1 = tuple((i[0], i[1]) for i in rows for i[0] in range(3))
-
-
-
-
-        subgrid01 = tuple((j[0], j[1]) for j in a for j[0] in range(3))
-
+    def check_subgrid_unique(self: object, cell: tuple, num: int) -> bool:
+        '''
+        Check if a given number already exists in a subgrid
+        :param cell: Coordinates of a cell
+        :param num: Checked number
+        :return: True/False
+        '''
+        subgrid_values = []
+        for subgrid in self.subgrids:
+            if cell in self.subgrids[subgrid]:
+                for subcell in self.subgrids[subgrid]:
+                    subgrid_values.append(self.get_cell_value(subcell))
+            if len(subgrid_values) > 0:
+                break
+        return not num in set(subgrid_values)
 
 
 class Solver:
 
     def solver(grid: object):
-        empty_cells = grid.get_empty_cells()
+        grid.get_empty_cells()
+        grid.get_subgrids()
+
         available_numbers = {}
-        for cell in empty_cells:
+
+        for cell in grid.empty_cells:
             available_numbers[cell] = list(range(1, grid.size+1))
 
+        i = 0
         try:
-            i = 0
-            while i < len(empty_cells):
-                cell = empty_cells[i]
+            while i < len(grid.empty_cells):
+                cell = grid.empty_cells[i]
                 j = 1
                 while j < grid.size + 1:
 
                     if j in available_numbers[cell]:
-                        if grid.check_row_unique(cell[0], j) and grid.check_column_unique(cell[1], j):
+                        if grid.check_row_unique(cell[0], j) and grid.check_column_unique(cell[1], j) \
+                                and grid.check_subgrid_unique(cell, j):
                             grid.update_cell_value(cell, j)
                             break
 
                         available_numbers[cell].remove(j)
 
                     if len(available_numbers[cell]) == 0:
-                        available_numbers[cell] = list(range(1, grid.size + 1))
-                        cell = empty_cells[empty_cells.index(cell)-1]
-
-                        i -= 1
-                        j = grid.get_cell_value(cell)
-                        available_numbers[cell].remove(j)
-
+                        cell, n, j, available_numbers = grid.rollback_cell_value(cell, available_numbers)
+                        i -= n
                     j += 1
                 i += 1
-        except ValueError:
-            print('This Sudoku board can not be solved')
+        except RecursionError as re:
+            print('{0}'.format(re.args[0]))
         else:
             print('Done')
+            print('\n'.join(' '.join(str(col) for col in row) for row in grid.grid))
 
 
 if __name__ == "__main__":
 
+    # grid = (
+    #     [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    #     [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    #     [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    #     [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    #     [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    #     [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    #     [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    #     [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    #     [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    # )
+
     grid = (
-        [5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        [4, 0, 3, 0, 0, 2, 0, 0, 0],
+        [5, 0, 0, 0, 6, 0, 1, 2, 0],
+        [9, 0, 0, 0, 0, 0, 0, 0, 4],
+        [0, 0, 8, 0, 7, 0, 0, 0, 0],
+        [0, 0, 0, 2, 0, 3, 0, 0, 8],
+        [0, 3, 6, 0, 0, 0, 7, 0, 0],
+        [0, 7, 0, 9, 2, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 5, 0, 9, 6],
+        [0, 0, 0, 8, 0, 4, 5, 0, 0]
     )
 
     # grid = (
@@ -182,24 +230,9 @@ if __name__ == "__main__":
     #     [0, 0, 1]
     # )
 
-    grid = Grid(grid)
-    grid.get_subgrid_coordinates()
-
-    a = grid.get_empty_cells()
-
-    a = grid.get_cell_value((1, 1))
-
-    a = grid.get_sum_subgrid()
-
-    a = grid.get_column(1)
-
-    a = grid.get_row(1)
-
-    a = grid.check_row_unique(1, 1)
-
-    Solver.solver(grid)
+    Solver.solver(Grid(grid))
 
     #sudoku.solver()
 
-    print('\n'.join(' '.join(str(col) for col in row) for row in grid.grid))
+
     # a = sudoku.check_row_unique()
